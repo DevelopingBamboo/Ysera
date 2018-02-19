a0f36c05bdafed6ade58a2afe2fd7c8a08de9bb4
Panda <pandapuntes@gmail.com>
2018-02-19 20:51:53 +0100

    Update musicbot.js

--- musicbot.js
+++ musicbot.js
@@ -12,7 +12,7 @@
 				//    You should have received a copy of the GNU General Public License       //   
 				//    along with this program.  If not, see <http://www.gnu.org/licenses/>.   //   
 				////////////////////////////////////////////////////////////////////////////////
-
+console.log('Pues ya estaría');
 const Discord = require("discord.js");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
