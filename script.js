9567bf4bec69374843ae2d504d58883b784fd5a9
Panda <pandapuntes@gmail.com>
2018-02-19 20:26:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+```js
+var bot = require("discord-music-bot");
+
+var serverName = "Elune-Sanguino";
+var textChannelName = "bot_music";
+var voiceChannelName = "Sala de musica";
+var aliasesFile = "";
+var botToken = "NDE0NjI1NjI4MjI3MjM5OTM2.DWqGxA.l7jJUO3pn2I91r7otQ11B16uQ80";
+
+bot.setYoutubeKey("AIzaSyCqMp5uIP5QgGJFIk-9DlKhRc4vJ8IfjiA");
+bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
+```
